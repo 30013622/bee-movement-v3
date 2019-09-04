f2b8c3aff79370c7597e48136da82bfb081c62ee
30013622 <briannacastano12@gmail.com>
2019-09-04 13:57:27 -0700

    made BeeWin level

---
+++ BeeWin.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class BeeWin here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class BeeWin extends MyWorld
+{
+
+    /**
+     * Constructor for objects of class BeeWin.
+     * 
+     */
+    public BeeWin()
+    {
+    }
+}
