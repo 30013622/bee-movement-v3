1d1b04daaa9c8b99b71b1e355ae0d11395681ae8
30013622 <briannacastano12@gmail.com>
2019-09-04 13:52:14 -0700

    input my name

--- MyWorld.java
+++ MyWorld.java
@@ -1,5 +1,5 @@
         import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-//Bri the cool kid
+//Bri the cool kid!!!!
 /**
  * Write a description of class MyWorld here.
  * 
